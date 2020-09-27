920fb9004d1d1a722f148e745d4656f1f8847fe7
Christian Cook <3473396+CookieCookson@users.noreply.github.com>
2020-09-27 17:41:44 +0100

    added getStatements

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,5 +1,5 @@
 import { Component, h, State } from "@stencil/core";
-import XAPI, { Agent } from "@xapi/xapi";
+import XAPI from "@xapi/xapi";
 import { Versions } from "@xapi/xapi/dist/types/constants";
 
 @Component({
@@ -10,16 +10,34 @@ export class AppRoot {
   private versionSelect!: HTMLSelectElement;
   private consoleEl: HTMLPreElement;
 
+  @State() xapi: XAPI;
+
   @State() endpointValue: string = "";
   @State() authorizationValue: string = "";
+
   @State() agentValue: string = "";
+
   @State() statementIdValue: string = "";
   @State() attachmentsValue: boolean = false;
+
   @State() voidedStatementIdValue: string = "";
   @State() voidedAttachmentsValue: boolean = false;
+
+  @State() getStatementsAgentValue: string = "";
+  @State() getStatementsVerbValue: string = "";
+  @State() getStatementsActivityValue: string = "";
+  @State() getStatementsRegistrationValue: string = "";
+  @State() getStatementsRelatedActivitiesValue: boolean = false;
+  @State() getStatementsRelatedAgentsValue: boolean = false;
+  @State() getStatementsSinceValue: string = "";
+  @State() getStatementsUntilValue: string = "";
+  @State() getStatementsLimitValue: string = "";
+  @State() getStatementsAttachmentsValue: boolean = false;
+  @State() getStatementsAscendingValue: boolean = false;
+  @State() getStatementsFormatValue: string = "";
+
   @State() consoleText: string = "";
 
-  @State() xapi: XAPI;
 
   getSearchQueryParamsAsObject() {
     const searchQueryParamsAsObject = XAPI.getSearchQueryParamsAsObject(location.href);
@@ -70,7 +88,7 @@ export class AppRoot {
   }
 
   getAgent() {
-    this.xapi.getAgent(JSON.parse(this.agentValue) as Agent)
+    this.xapi.getAgent(JSON.parse(this.agentValue))
       .then((result) => {
         this.addToConsole('getAgent()', JSON.stringify(result.data, null, 2));
       })
@@ -83,14 +101,49 @@ export class AppRoot {
     this.xapi.getStatement({
       statementId: this.statementIdValue,
       attachments: this.attachmentsValue
-    });
+    })
+      .then((result) => {
+        this.addToConsole('getStatement()', JSON.stringify(result.data, null, 2));
+      })
+      .catch((error) => {
+        this.addToConsole('getStatement()', JSON.stringify(error, null, 2));
+      });
   }
 
   getVoidedStatement() {
     this.xapi.getVoidedStatement({
       voidedStatementId: this.voidedStatementIdValue,
       attachments: this.voidedAttachmentsValue
-    });
+    })
+      .then((result) => {
+        this.addToConsole('getVoidedStatement()', JSON.stringify(result.data, null, 2));
+      })
+      .catch((error) => {
+        this.addToConsole('getVoidedStatement()', JSON.stringify(error, null, 2));
+      });
+  }
+
+  getStatements() {
+    this.xapi.getStatements({
+      ...(this.getStatementsAgentValue ? { agent: JSON.parse(this.getStatementsAgentValue) } : null),
+      ...(this.getStatementsVerbValue ? { verb: this.getStatementsVerbValue} : null),
+      ...(this.getStatementsActivityValue ? { activity: this.getStatementsActivityValue } : null),
+      ...(this.getStatementsRegistrationValue ? { registration: this.getStatementsRegistrationValue } : null),
+      ...(this.getStatementsRelatedActivitiesValue ? { related_activities: this.getStatementsRelatedActivitiesValue} : null),
+      ...(this.getStatementsRelatedAgentsValue ? { related_agents: this.getStatementsRelatedAgentsValue } : null),
+      ...(this.getStatementsSinceValue ? { since: this.getStatementsSinceValue } : null),
+      ...(this.getStatementsUntilValue ? { until: this.getStatementsUntilValue } : null),
+      ...(this.getStatementsLimitValue ? { limit: Number(this.getStatementsLimitValue) } : null),
+      ...(this.getStatementsAttachmentsValue ? { attachments: this.getStatementsAttachmentsValue } : null),
+      ...(this.getStatementsAscendingValue ? { ascending: this.getStatementsAscendingValue } : null),
+      ...(this.getStatementsFormatValue ? { format: this.getStatementsFormatValue as any } : null)
+    })
+      .then((result) => {
+        this.addToConsole('getStatements()', JSON.stringify(result.data, null, 2));
+      })
+      .catch((error) => {
+        this.addToConsole('getStatements()', JSON.stringify(error, null, 2));
+      });
   }
 
   addToConsole(label: string, output: string) {
@@ -178,7 +231,7 @@ export class AppRoot {
                   <textarea
                     value={this.agentValue}
                     onInput={(e) => this.agentValue = (e.target as HTMLTextAreaElement).value}
-                    rows={12}
+                    rows={8}
                     placeholder="{...}"
                   ></textarea>
                 </label>
@@ -193,7 +246,7 @@ export class AppRoot {
               </summary>
 
               {/* Get Statement */}
-              <details open>
+              <details>
                 <summary>
                   <h4>Get Statement</h4>
                   </summary>
@@ -242,6 +295,131 @@ export class AppRoot {
                   />
                 </label>
                 <button onClick={() => this.getVoidedStatement()} disabled={!this.xapi || !this.voidedStatementIdValue}>getVoidedStatement(...)</button>
+              </details>
+
+              {/* Get Statements */}
+              <details>
+                <summary>
+                  <h4>Get Statements</h4>
+                </summary>
+                <label>
+                  agent
+                  <textarea
+                    value={this.getStatementsAgentValue}
+                    onInput={(e) => this.getStatementsAgentValue = (e.target as HTMLTextAreaElement).value}
+                    rows={8}
+                    placeholder="{...}"
+                  ></textarea>
+                </label>
+                <label>
+                  verb
+                  <input
+                    type="text"
+                    value={this.getStatementsVerbValue}
+                    placeholder="http://adlnet.gov/expapi/verbs/experienced"
+                    onInput={(e) => this.getStatementsVerbValue = (e.target as HTMLInputElement).value}
+                    autoCorrect={"false"}
+                    autoCapitalize={"false"}
+                  />
+                </label>
+                <label>
+                  activity
+                  <input
+                    type="text"
+                    value={this.getStatementsActivityValue}
+                    placeholder="https://www.xapijs.dev/activity/xapijs/xapi-demo"
+                    onInput={(e) => this.getStatementsActivityValue = (e.target as HTMLInputElement).value}
+                    autoCorrect={"false"}
+                    autoCapitalize={"false"}
+                  />
+                </label>
+                <label>
+                  registration
+                  <input
+                    type="text"
+                    value={this.getStatementsRegistrationValue}
+                    onInput={(e) => this.getStatementsRegistrationValue = (e.target as HTMLInputElement).value}
+                    autoCorrect={"false"}
+                    autoCapitalize={"false"}
+                  />
+                </label>
+                <label>
+                  related_activities
+                  <input
+                    type="checkbox"
+                    checked={this.getStatementsRelatedActivitiesValue}
+                    onInput={(e) => this.getStatementsRelatedActivitiesValue = (e.target as HTMLInputElement).checked}
+                  />
+                </label>
+                <label>
+                  related_agents
+                  <input
+                    type="checkbox"
+                    checked={this.getStatementsRelatedAgentsValue}
+                    onInput={(e) => this.getStatementsRelatedAgentsValue = (e.target as HTMLInputElement).checked}
+                  />
+                </label>
+                <label>
+                  since
+                  <input
+                    type="text"
+                    value={this.getStatementsSinceValue}
+                    onInput={(e) => this.getStatementsSinceValue = (e.target as HTMLInputElement).value}
+                    autoCorrect={"false"}
+                    autoCapitalize={"false"}
+                  />
+                </label>
+                <label>
+                  until
+                  <input
+                    type="text"
+                    value={this.getStatementsUntilValue}
+                    onInput={(e) => this.getStatementsUntilValue = (e.target as HTMLInputElement).value}
+                    autoCorrect={"false"}
+                    autoCapitalize={"false"}
+                  />
+                </label>
+                <label>
+                  limit
+                  <input
+                    type="number"
+                    min="1"
+                    step="1"
+                    value={this.getStatementsLimitValue}
+                    onInput={(e) => this.getStatementsLimitValue = (e.target as HTMLInputElement).value}
+                    autoCorrect={"false"}
+                    autoCapitalize={"false"}
+                  />
+                </label>
+                <label>
+                  attachments
+                  <input
+                    type="checkbox"
+                    checked={this.getStatementsAttachmentsValue}
+                    onInput={(e) => this.getStatementsAttachmentsValue = (e.target as HTMLInputElement).checked}
+                  />
+                </label>
+                <label>
+                  ascending
+                  <input
+                    type="checkbox"
+                    checked={this.getStatementsAscendingValue}
+                    onInput={(e) => this.getStatementsAscendingValue = (e.target as HTMLInputElement).checked}
+                  />
+                </label>
+                <label>
+                  format
+                  <select onChange={(e) => {
+                    const target = e.target as HTMLSelectElement;
+                    this.getStatementsFormatValue = target.options[target.selectedIndex].value;
+                  }}>
+                    <option value=""></option>
+                    <option value="exact">exact</option>
+                    <option value="ids">ids</option>
+                    <option value="canonical">canonical</option>
+                  </select>
+                </label>
+                <button onClick={() => this.getStatements()} disabled={!this.xapi}>getStatements(...)</button>
               </details>
 
             </details>
